cc750c83995afdd17590324a90af69499939dc5a
0ly <rolon724@yahoo.com>
2016-06-30 11:04:32 -0400

    Paint splatter (initial)

---
+++ Advanced_JS_ Natural Simulations/Randomness/paint_splatter.js
@@ -0,0 +1,4 @@
+/*Consider a simulation of paint splatter drawn as a collection of colored dots. Most of the paint clusters around a central location, but some dots do splatter out towards the edges. In this project, use what you've learnt to draw the paint splatter:
+
+Use a normal distribution of random numbers to generate the locations of the dots.
+Bonus: use a normal distribution of random numbers to generate a color palette.*/

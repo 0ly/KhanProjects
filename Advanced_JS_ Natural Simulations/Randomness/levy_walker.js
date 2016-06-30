9edabeeb41aca9daa44f495fe5623da9e9b70988
0ly <rolon724@yahoo.com>
2016-06-30 10:55:55 -0400

    Levy Walker (Initial)

---
+++ Advanced_JS_ Natural Simulations/Randomness/levy_walker.js
@@ -0,0 +1,42 @@
+// Generates random numbers using the Monte Carlo Method
+/*The steps of this walker follow an uniform distribution - all step lengths are equally likely - as they are based on the random function. We want you to change it to a probability distribution - smaller steps are more likely - by using the values from the monteCarlo function instead.*/
+
+var monteCarlo = function() {
+    while (true) {
+        var r1 = random(1);
+        var probability = r1;
+        var r2 = random(1);
+        if (r2 < probability) {
+            return r1;
+        }
+    }
+};
+
+var Walker = function() {
+    this.x = width/2;
+    this.y = height/2;
+};
+
+Walker.prototype.display = function() {
+    strokeWeight(3);
+    stroke(0, 0, 0);
+    point(this.x, this.y);
+};
+
+// Randomly move up, down, left, right, or stay in one place
+Walker.prototype.walk = function() {
+    var stepSize = random(0, 10);
+ 
+    var xStepSize = random(-stepSize, stepSize);
+    var yStepSize = random(-stepSize, stepSize);
+ 
+    this.x += xStepSize;
+    this.y += yStepSize;
+};
+
+var w = new Walker();
+
+draw = function() {
+    w.walk();
+    w.display();
+};

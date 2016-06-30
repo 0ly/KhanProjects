9a5517a066bd9ba56627970627fdcd4ad2ee4c90
0ly <rolon724@yahoo.com>
2016-06-30 11:02:49 -0400

    Step 2:  Make it prefer short distances (complete)

--- Advanced_JS_ Natural Simulations/Randomness/levy_walker.js
+++ Advanced_JS_ Natural Simulations/Randomness/levy_walker.js
@@ -1,7 +1,11 @@
 // Generates random numbers using the Monte Carlo Method
 /*The steps of this walker follow an uniform distribution - all step lengths are equally likely - as they are based on the random function. We want you to change it to a probability distribution - smaller steps are more likely - by using the values from the monteCarlo function instead.*/
 
-/*Step 1: Use the Monte Carlo method*/
+//Step 1: Use the Monte Carlo method
+
+//Step 2: Make it prefer short distances
+/* This monteCarlo function tends to yield higher numbers, but we want a Levy walk, which should be more likely to yield lower numbers. Change the monteCarlo function so that the probability is higher for lower numbers instead. Hint: 1 character is all you need to change.*/
+
 
 
 var monteCarlo = function() {
@@ -9,7 +13,7 @@ var monteCarlo = function() {
         var r1 = random(1);
         var probability = r1;
         var r2 = random(1);
-        if (r2 < probability) {
+        if (r2 > probability) {
             return r1;
         }
     }

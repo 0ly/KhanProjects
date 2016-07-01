6d11215ee6fe8472e749d945aede1a64ab268630
0ly <rolon724@yahoo.com>
2016-07-01 13:03:12 -0400

    Step 1: Use noise for the step size (Complete)

--- Advanced_JS_ Natural Simulations/Noise/noisy_step_walker.js
+++ Advanced_JS_ Natural Simulations/Noise/noisy_step_walker.js
@@ -1,10 +1,14 @@
-/*Use noise for the step size
+/*This walker takes a random step each time. Change it to use a noise-based step size instead. The best way to do this is to setup two timer properties for x and y that you increment each time, and map to a reasonable value.*/
 
-This walker takes a random step each time. Change it to use a noise-based step size instead. The best way to do this is to setup two timer properties for x and y that you increment each time, and map to a reasonable value.*/
+/*Step 1: Use noise for the step size*/
 
 var Walker = function() {
     this.x = width/2;
     this.y = height/2;
+
+    //noise time   
+    this.tx = 0;
+    this.ty = 10000;
 };
 
 Walker.prototype.display = function() {
@@ -15,11 +19,17 @@ Walker.prototype.display = function() {
 
 Walker.prototype.walk = function() {
  
-    var xStepSize = random(-5, 5);
-    var yStepSize = random(-5, 5);
+    //mapping noise to stepsize
+    var xStepSize = map(noise(this.tx), 0, 1, 0, width);
+    var yStepSize = map(noise(this.ty), 0, 1, 0, height);
+    
+    //adjusting x/y positions
+    this.x = xStepSize;
+    this.y = yStepSize;
 
-    this.x += xStepSize;
-    this.y += yStepSize;
+    //incrementing time for noise
+    this.tx += 0.01;
+    this.ty += 0.01;
 };
 
 var w = new Walker();

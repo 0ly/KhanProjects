79e8c93eb8619445c765451e043a66a8b6335499
0ly <rolon724@yahoo.com>
2016-07-02 22:45:24 -0400

    Step 2: Use a PVector for the step size (Complete)

--- Advanced_JS_ Natural Simulations/Vectors/vector_walker.js
+++ Advanced_JS_ Natural Simulations/Vectors/vector_walker.js
@@ -3,6 +3,9 @@
 /*Step 1: Use a PVector for the position*/
 /*You will need to change the constructor and the methods, since they all use the position in some way.*/
 
+/*Step 2: Use a PVector for the step size*/
+/*The Walker calculates a step size in each direction and adds that to the x and y of the position. Use a PVector to store the step size instead, and use the add method of the position vector to come up with the new position.*/
+
 var Walker = function() {
     
     this.position = new PVector(width/2, height/2);
@@ -16,11 +19,8 @@ Walker.prototype.display = function() {
 
 Walker.prototype.walk = function() {
  
-    var xStepSize = random(-5, 5);
-    var yStepSize = random(-5, 5);
-    
-    this.position.x += xStepSize;
-    this.position.y += yStepSize;
+    var velocity = new PVector(random(-5,5), random(-5,5));
+    this.position.add(velocity);
 };
 
 var w = new Walker();

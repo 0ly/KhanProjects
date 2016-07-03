76e505b272175a162df7d06cae04553c9a699ada
0ly <rolon724@yahoo.com>
2016-07-03 15:31:40 -0400

    Step 1: Multply!

--- Advanced_JS_ Natural Simulations/Vectors/static_functions.js
+++ Advanced_JS_ Natural Simulations/Vectors/static_functions.js
@@ -1,3 +1,7 @@
 /*Practice using the static functions on PVector. */
 
+/*Step 1: Multply!*/
+/*Start by creating a new vector u that's equal to v multiplied by 2.*/
+
 var v = new PVector(1,5);
+var u = PVector.mult(v,2);

8d41e7a6927b29405ec361e0878f472bfbc6700e
0ly <rolon724@yahoo.com>
2016-07-02 22:39:58 -0400

    Step 1: Use a PVector for the position

--- Advanced_JS_ Natural Simulations/Vectors/vector_walker.js
+++ Advanced_JS_ Natural Simulations/Vectors/vector_walker.js
@@ -1,14 +1,17 @@
 /*This Walker uses separate properties, x and y, for the position. Change it so it stores a single position property, a PVector, that behaves the same. */
 
+/*Step 1: Use a PVector for the position*/
+/*You will need to change the constructor and the methods, since they all use the position in some way.*/
+
 var Walker = function() {
-    this.x = width/2;
-    this.y = height/2;
+    
+    this.position = new PVector(width/2, height/2);
 };
 
 Walker.prototype.display = function() {
     strokeWeight(3);
     stroke(0, 0, 0);
-    point(this.x, this.y);
+    point(this.position.x, this.position.y);
 };
 
 Walker.prototype.walk = function() {
@@ -16,8 +19,8 @@ Walker.prototype.walk = function() {
     var xStepSize = random(-5, 5);
     var yStepSize = random(-5, 5);
     
-    this.x += xStepSize;
-    this.y += yStepSize;
+    this.position.x += xStepSize;
+    this.position.y += yStepSize;
 };
 
 var w = new Walker();

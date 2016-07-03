000a2759244eb1a14486dc548c670d1bee762850
0ly <rolon724@yahoo.com>
2016-07-03 01:49:49 -0400

    Step 1: Display the magnitude

--- Advanced_JS_ Natural Simulations/Vectors/magnitude_visualizer.js
+++ Advanced_JS_ Natural Simulations/Vectors/magnitude_visualizer.js
@@ -1,5 +1,10 @@
 /*This program draws a vector to represent the position of the mouse relative to the top left corner.*/
 
+/*Step 1: Display the magnitude*/
+/*Use the text command to display the magnitude of that vector at the end of it.*/
+
+
+
 mouseMoved = function() {
     background(255, 255, 255);
     
@@ -8,4 +13,7 @@ mouseMoved = function() {
     stroke(255, 0, 0);
     strokeWeight(3);
     line(0, 0, mouse.x, mouse.y);
+    var mag = mouse.mag();
+    fill(63, 26, 212);
+    text(mag, mouse.x, mouse.y);
 };

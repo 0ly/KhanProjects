afba7855c289bd2598c71bf4f5b4bca38dcdf53c
0ly <rolon724@yahoo.com>
2016-07-02 23:15:08 -0400

    Step 2: Make it shrink(Complete)

--- Advanced_JS_ Natural Simulations/Vectors/lightsaber.js
+++ Advanced_JS_ Natural Simulations/Vectors/lightsaber.js
@@ -1,6 +1,8 @@
 /*This light saber is drawn according to the coordinates of a vector.*/
 /*Step 1: Make it grow!*/
 /*Use the keyPressed function to make it double in size every time the user presses the up arrow, by multiplying the vector.*/
+/*Step 2: Make it shrink*/
+/*Now, make it so that every time you press the down arrow, the light saber shrinks in half, by dividing the vector.*/
 
 var  v = new PVector(50, 75);
 
@@ -19,10 +21,13 @@ var drawSaber = function() {
 drawSaber();
 
 keyPressed = function(){
-        if(keyCode===38){
+        if(keyCode === 38){
             
             v.mult(2);
             //println(v.x + " " + v.y);
+        } else if(keyCode === 40){
+            //println(v.x + " " + v.y);
+            v.div(2);
         }
         drawSaber();
     };

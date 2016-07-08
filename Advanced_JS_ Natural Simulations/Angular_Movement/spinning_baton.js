a0fd06b19098f937284f7eacc97ad9be1f8d0b09
0ly <rolon724@yahoo.com>
2016-07-07 21:50:37 -0400

    Step 1: Center the baton

--- Advanced_JS_ Natural Simulations/Angular_Movement/spinning_baton.js
+++ Advanced_JS_ Natural Simulations/Angular_Movement/spinning_baton.js
@@ -1,14 +1,24 @@
 /*This program draws a baton in the upper left.*/
 
+/*Step 1: Center the baton*/
+/*Use the translate function to center it in the middle of the canvas.*/
+
 angleMode = "radians";	
 
 draw = function() {
     background(255);
     
+    pushMatrix();
+
+    translate(200,200);
+
     fill(127, 127, 127);
     stroke(0, 0, 0);
     line(-50, 0, 50, 0);
     strokeWeight(2);
     ellipse(-50, 0, 16, 16);
     ellipse(50, 0, 16, 16);
+
+    popMatrix();
+
 };

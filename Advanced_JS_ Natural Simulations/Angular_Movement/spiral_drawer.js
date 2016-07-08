680301b774f5667822cb49428e7bbeaa8638978d
0ly <rolon724@yahoo.com>
2016-07-08 12:01:49 -0400

    Step 1: Draw a spiral (Complete)

--- Advanced_JS_ Natural Simulations/Angular_Movement/spiral_drawer.js
+++ Advanced_JS_ Natural Simulations/Angular_Movement/spiral_drawer.js
@@ -1,10 +1,12 @@
 /*This program animates a pendulum going around and around. */
+//Step 1: Draw a spiral
 
-var r = 150;
+/*Can you change a few lines of code so that it draws a spiral, starting from the center and growing outward?*/
+
+var r = 0;
 var theta = 0;
 
 draw = function() {
-    background(255, 255, 255);
     
     pushMatrix();
     translate(width/2, height/2);
@@ -13,9 +15,10 @@ draw = function() {
     var y = r * sin(theta);
     
     fill(0, 0, 0);
-    line(0, 0, x, y);
     ellipse(x, y, 10, 10);
     popMatrix();
     
     theta += 1;
+    
+    r+=0.1;
 };

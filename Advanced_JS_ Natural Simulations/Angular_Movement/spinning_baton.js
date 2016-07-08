75a7f51d7044ccfd6ed7d1721a9685c738a170dd
0ly <rolon724@yahoo.com>
2016-07-07 21:56:43 -0400

    Step 2: Spin the baton! (Complete)

--- Advanced_JS_ Natural Simulations/Angular_Movement/spinning_baton.js
+++ Advanced_JS_ Natural Simulations/Angular_Movement/spinning_baton.js
@@ -3,7 +3,12 @@
 /*Step 1: Center the baton*/
 /*Use the translate function to center it in the middle of the canvas.*/
 
+/*Step 2: Spin the baton!*/
+/*  
+Now that the baton is in the middle, spin it around its center, rotate()ing it a little bit every time draw is called.*/
+
 angleMode = "radians";	
+var angle = 0;
 
 draw = function() {
     background(255);
@@ -11,7 +16,8 @@ draw = function() {
     pushMatrix();
 
     translate(200,200);
-
+    rotate(angle);
+    angle+=0.03;
     fill(127, 127, 127);
     stroke(0, 0, 0);
     line(-50, 0, 50, 0);

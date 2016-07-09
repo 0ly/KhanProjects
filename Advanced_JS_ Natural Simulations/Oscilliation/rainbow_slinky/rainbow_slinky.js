dddc697031d9d3dd7e40c0d136a9c2ed532213b2
0ly <rolon724@yahoo.com>
2016-07-09 11:40:50 -0400

    Step 1: Slink that slinky! (Complete)

--- Advanced_JS_ Natural Simulations/Oscilliation/rainbow_slinky/rainbow_slinky.js
+++ Advanced_JS_ Natural Simulations/Oscilliation/rainbow_slinky/rainbow_slinky.js
@@ -1,5 +1,8 @@
 /*This slinky is hanging from the ceiling, so it should really be bobbing up and down from spring forces.*/
 
+/*Step 1: Slink that slinky!*/
+/*Use the sin() function to calculate the y position of the bottom of the slinky, and map() to convert it to a reasonable value.*/
+
 angleMode = "radians";
 
 var drawSlinky = function(centerX, startY, endY) {
@@ -16,8 +19,9 @@ var drawSlinky = function(centerX, startY, endY) {
 };
 
 draw = function() {
-    
+    var y = sin(TWO_PI * frameCount / 10);
+    var m = map(y,-1,1,100,200);
     
     background(255);
-    drawSlinky(width/2, 10, 200);
+    drawSlinky(width/2, 10, m);
 };

6895b4fe2ff04ce836607e25cb4f4deffc86db60
mikey <mikeyjhavoc@gmail.com>
2016-07-09 10:07:46 -0400

    comments for falling leaves.
    add comments for falling leaves.

---
+++ Advanced_JS_ Natural Simulations/Oscilliation/particle_system/falling_leaves.js
@@ -0,0 +1,4 @@
+//step one
+/*
+One common use of particle systems is to simulate falling leaves. We've drawn a tree in this program and included a leaf-like Particle object in the code, and want you to make it so that clicking somewhere will make a leaf fall from that point.
+*/

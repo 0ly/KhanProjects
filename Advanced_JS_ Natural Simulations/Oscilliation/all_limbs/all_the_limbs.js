075aadc20b8ee1f8b818320d707d7efe6cdbe20f
mikey <mikeyjhavoc@gmail.com>
2016-07-09 07:59:10 -0400

    all limbs comments
    all the limbs comments added.

---
+++ Advanced_JS_ Natural Simulations/Oscilliation/all_limbs/all_the_limbs.js
@@ -0,0 +1,3 @@
+/*
+In this challenge, you'll make a puppet out of pendulums. We've created all the limbs and stored them in a variable, but we're only displaying and responding to mouse events for one of them right now. Make it so we do that for all of the limbs instead.
+*/

76d7226210d4c92b2b893c0e1313c270479f2f99
mikey <mikeyjhavoc@gmail.com>
2016-07-09 06:39:51 -0400

    step one comments
    comments for step one.

---
+++ Advanced_JS_ Natural Simulations/Oscilliation/spaceship_ride/many_waves.js
@@ -0,0 +1,4 @@
+/*
+step one
+This program defines a Wave object, so that we can create multiples waves with different periods and amplitudes easily. Like we did in our example, display the wave as a series of ellipses moving across the screen.
+*/

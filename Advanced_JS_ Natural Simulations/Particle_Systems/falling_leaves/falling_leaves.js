301f30505a136b48525b294d63e789f9d5d0bac5
0ly <rolon724@yahoo.com>
2016-07-09 13:47:12 -0400

    Step 3: Spin the leaves (Complete)

--- Advanced_JS_ Natural Simulations/Particle_Systems/falling_leaves/falling_leaves.js
+++ Advanced_JS_ Natural Simulations/Particle_Systems/falling_leaves/falling_leaves.js
@@ -1,19 +1,24 @@
-/*One common use of particle systems is to simulate falling leaves. We've drawn a tree in this program and included a leaf-like Particle object in the code, and want you to make it so that clicking somewhere will make a leaf fall from that point.*/
-
-//Step 1: Add a falling leaf
-/*"make it so that clicking somewhere will make a leaf fall from that point"*/
-
-//Step 2: Make the leaves pile up
-/*Right now, your leaves are falling off screen forever, because we didn't give them a lifespan or any stopping mechanism. Since they're leaves, they should probably stop falling once they hit the ground, right? Add conditional logic so that the leaves pile up on the bottom of the screen.*/
-
-
+ //Step 1: Add a falling leaf
+  /*"make it so that clicking somewhere will make a leaf fall from that point"*/		  /*"make it so that clicking somewhere will make a leaf fall from that point"*/
+  		  
+ //Step 2: Make the leaves pile up
+ /*Right now, your leaves are falling off screen forever, because we didn't give them a lifespan or any stopping mechanism. Since they're leaves, they should probably stop falling once they hit the ground, right? Add conditional logic so that the leaves pile up on the bottom of the screen.*/
+ 
+ //Step 3: Spin the leaves
+ /*Leaves usually spin as they fall. Use the technique from the angular velocity section to make the leaves spin as they fall from the tree (not too fast!).*/
 
 angleMode = "radians";
 
 var Particle = function(position) {
+
     this.acceleration = new PVector(0, 0.05);
     this.velocity = new PVector(random(0, 1), random(0, 0));
     this.position = position;
+
+    //properties for leaf spin
+    this.angle = 0;
+    this.aVelocity = 0;
+    this.aAcceleration = 0.001;
 };
 
 Particle.prototype.run = function() {
@@ -24,10 +29,19 @@ Particle.prototype.run = function() {
 Particle.prototype.update = function(){
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
+
+    //increment values to spin
+    this.aVelocity += this.aAcceleration;
+    this.angle += this.aVelocity;
+
 };
 
 Particle.prototype.display = function() {
-    image(getImage("avatars/leaf-green"), this.position.x, this.position.y, 40, 40);
+   pushMatrix();
+    translate(this.position.x, this.position.y);
+    rotate(this.angle);
+    image(getImage("avatars/leaf-green"), -20, -20, 40, 40);
+    popMatrix();
 };
 
 var Tree = function(position, options) {
@@ -103,6 +117,10 @@ draw = function() {
          if(leaf.position.y>=height-20) {
             leaf.velocity.set(0, 0);
             leaf.acceleration.set(0, 0);
+
+            //zero out to stop spinning
+            leaf.aAcceleration = 0;
+            leaf.aVelocity = 0;
         }
     }
 };

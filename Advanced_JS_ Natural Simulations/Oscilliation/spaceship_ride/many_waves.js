cc87edcc11ff3e5d072bea82928e92aeb74588b8
mikey <mikeyjhavoc@gmail.com>
2016-07-09 06:57:09 -0400

    step one wave
    step one of wave complete.

--- Advanced_JS_ Natural Simulations/Oscilliation/spaceship_ride/many_waves.js
+++ Advanced_JS_ Natural Simulations/Oscilliation/spaceship_ride/many_waves.js
@@ -1,8 +1,3 @@
-/*
-step one
-This program defines a Wave object, so that we can create multiples waves with different periods and amplitudes easily. Like we did in our example, display the wave as a series of ellipses moving across the screen.
-*/
-
 angleMode = "radians";
 
 var Wave = function(amplitude, period, color) {
@@ -16,8 +11,36 @@ var Wave = function(amplitude, period, color) {
 Wave.prototype.update = function() {
     this.startAngle += TWO_PI / this.period;
 };
+/*
+/*
+ startAngle += 0.015;
+  var angle = startAngle;
 
+  for (var x = 0; x <= width; x += 24) {
+    // Calculate the y location according to amplitude and sine of the angle.
+    var y = amplitude * sin(angle);
+    // Draw a circle at the x, y location
+    stroke(0, 0, 0);
+    fill(0, 0, 0, 50);
+    strokeWeight(2);
+    ellipse(x, y+height/2, 48, 48);
+    // Increment the angle according to angular velocity
+    angle += angleVel;
+  }
+*/
 Wave.prototype.draw = function() {
+    //step one
+    var angle = this.startAngle;
+    fill(this.color);
+
+    for(var x = 0; x <= width; x += 24) {
+        var y = this.amplitude * sin(angle);
+        ellipse(x , y + height / 2, 48, 48);
+        angle += this.angleVel;
+
+
+    }
+    //end step one
 };
 
 var wave = new Wave(200, 175, color(255, 0, 0, 100));

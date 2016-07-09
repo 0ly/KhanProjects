334845a0d34e3a2118a3bb77e11a067b7db5b368
mikey <mikeyjhavoc@gmail.com>
2016-07-09 09:08:08 -0400

    connect limbs step 2
    connect limbs puppet step 2 complete.

--- Advanced_JS_ Natural Simulations/Oscilliation/all_limbs/all_the_limbs.js
+++ Advanced_JS_ Natural Simulations/Oscilliation/all_limbs/all_the_limbs.js
@@ -1,7 +1,11 @@
 /*
+step one
 In this challenge, you'll make a puppet out of pendulums. We've created all the limbs and stored them in a variable, but we're only displaying and responding to mouse events for one of them right now. Make it so we do that for all of the limbs instead.
 */
-
+//step two
+/*
+The parts of the limbs move independently now - try dragging them and you'll see. What we want instead is for the bottom part of each limb to connect to the top limb, like a joint. That means that for half the pendulums, the origin isn't static - it varies, based on their parent pendulum's position. To make this happen in code, we suggest letting the first argument of Pendulum be either a PVector or another Pendulum, and acting accordingly. But there may be other ways, too. (Note that this looks like what's called a 'double pendulum', but truly simulating a double pendulum requires far more complex equations.)
+*/
 angleMode = "radians";
 
 var Pendulum  = function(origin, armLength) {
@@ -39,18 +43,28 @@ Pendulum.prototype.update = function() {
 };
 
 Pendulum.prototype.display = function() {
+    //step two change this section. \/
+     if (this.origin instanceof PVector) {
+        this.currentOrigin = this.origin;
+    } else {
+        this.currentOrigin = this.origin.position;
+    }
+    // step 2 all origins here too currentOrigin.
     this.position = new PVector(
        this.armLength * sin(this.angle),
        this.armLength * cos(this.angle));
-    this.position.add(this.origin);
+    this.position.add(this.currentOrigin);
     stroke(0, 0, 0);
     strokeWeight(3);
-    line(this.origin.x, this.origin.y, this.position.x, this.position.y);
+    line(this.currentOrigin.x, this.currentOrigin.y, this.position.x, this.position.y);
     fill(224, 194, 134);
     if (this.dragging) {
         fill(143, 110, 44);
     }
     ellipse(this.position.x, this.position.y, this.ballRadius, this.ballRadius);
+
+
+
 };
 
 Pendulum.prototype.handleClick = function(mx, my) {
@@ -67,11 +81,12 @@ Pendulum.prototype.stopDragging = function() {
 
 Pendulum.prototype.handleDrag = function(mx, my) {
     if (this.dragging) {
-      var diff = PVector.sub(this.origin, new PVector(mx, my));
+                 //change step 2 origin -> currentOrigin
+      var diff = PVector.sub(this.currentOrigin, new PVector(mx, my));
       this.angle = atan2(-1*diff.y, diff.x) - radians(90);
     }
 };
-
+/*
 var limbLength = 75;
 var leftArm1  = new Pendulum(new PVector(width/2-50, 110), limbLength);
 var leftArm2  = new Pendulum(new PVector(width/2-50, 185), limbLength);
@@ -81,6 +96,20 @@ var leftLeg1  = new Pendulum(new PVector(width/2+40, 230), limbLength);
 var leftLeg2  = new Pendulum(new PVector(width/2+40, 305), limbLength);
 var rightLeg1 = new Pendulum(new PVector(width/2-40, 230), limbLength);
 var rightLeg2 = new Pendulum(new PVector(width/2-40, 305), limbLength);
+*/
+
+//step 2
+var limbLength = 75;
+var leftArm1  = new Pendulum(new PVector(width/2-50, 110), limbLength,2);
+var leftArm2  = new Pendulum(leftArm1, limbLength,30);
+var rightArm1 = new Pendulum(new PVector(width/2+50, 110), limbLength,2);
+var rightArm2 = new Pendulum(rightArm1, limbLength,30);
+var leftLeg1  = new Pendulum(new PVector(width/2+40, 230), limbLength,2);
+var leftLeg2  = new Pendulum(leftLeg1, limbLength,30);
+var rightLeg1 = new Pendulum(new PVector(width/2-40, 230), limbLength,2);
+var rightLeg2 = new Pendulum(rightLeg1, limbLength,30);
+
+
 
 var limbs = [leftLeg1, leftLeg2,
                  rightLeg1, rightLeg2,

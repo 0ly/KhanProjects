ba5d3832b840b80ddd626ba4ce015ce56b2d70ea
mikey <mikeyjhavoc@gmail.com>
2016-07-10 08:54:50 -0400

    step one, step two, step three of fish bubbles
    new clone of khanProjects. put in steps one, two, three. before
    completing step four.

---
+++ Advanced_JS_ Natural Simulations/Particle_Systems/fish_bubbles/fish_bubbles.js
@@ -0,0 +1,12 @@
+//step one
+/*
+This program draws a fish and bubbles coming out of its mouth, using a particle system for the bubbles. But, it's adding a new bubble in every frame, and that seems like a lot of bubbles to come out of one fish. Can you change it so that it only adds bubble every few frames, using the frameCount variable?
+*/
+//step two
+/*
+In the ocean, bubbles get bigger as they travel to the surface because there's less pressure and the gas expands. Add some logic to the particles so that they get bigger as they get closer to the surface.
+*/
+//step three
+/*
+Now that the bubbles are more realistic, make the fish more realistic - make it swim across the ocean, starting over on the left side whenever it goes beyond the right side of the canvas.
+*/

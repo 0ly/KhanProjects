f742f633e6a57d0fa5b80deccb2dda011e1a6f1d
mikey <mikeyjhavoc@gmail.com>
2016-07-10 12:57:33 -0400

    add scripts random walks file
    add scripts for techniques random walk to begin.

---
+++ scripts/randomness/walks/randomWalks.js
@@ -0,0 +1,44 @@
+//create a walker object
+/*
+var Walker = function() {
+    this.x = width/2;
+    this.y = height/2;
+};
+
+// display function
+
+Walker.prototype.display = function() {
+    stroke(0, 0, 0);
+    point(this.x, this.y);
+};
+
+// walk function
+
+Walker.prototype.walk = function() {
+    var choice = floor(random(4));
+};
+
+// movement functions
+
+Walker.prototype.walk = function() {
+    var choice = floor(random(4));
+    if (choice === 0) {
+        this.x++; //right
+    } else if (choice === 1) {
+        this.x--; //left
+    } else if (choice === 2) {
+        this.y++; //down
+    } else {
+        this.y--; //up
+    }
+};
+
+//create our Walker
+//var w = new Walker();
+//display
+
+draw = function() {
+    w.walk();
+    w.display();
+};
+*/

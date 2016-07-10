eb94141013997016aa7030f5571a8a69c4362ba3
mikey <mikeyjhavoc@gmail.com>
2016-07-10 12:59:08 -0400

    updated random walk
    random walk and advanced random walk.

--- scripts/randomness/walks/randomWalks.js
+++ scripts/randomness/walks/randomWalks.js
@@ -1,34 +1,19 @@
+// left right up down walker.
+//*****************************
+//*random left right up down walker*
 //create a walker object
 /*
 var Walker = function() {
-    this.x = width/2;
-    this.y = height/2;
+draw = function() {
 };
+*/
 
-// display function
-
-Walker.prototype.display = function() {
-    stroke(0, 0, 0);
-    point(this.x, this.y);
-};
-
-// walk function
-
+/*********************************************
+/*****************************************
+// improved random walker.
+//create a walker object
+/*
 Walker.prototype.walk = function() {
-    var choice = floor(random(4));
-};
-
-// movement functions
-
-Walker.prototype.walk = function() {
-    var choice = floor(random(4));
-    if (choice === 0) {
-        this.x++; //right
-    } else if (choice === 1) {
-        this.x--; //left
-    } else if (choice === 2) {
-        this.y++; //down
-    } else {
         this.y--; //up
     }
 };
@@ -38,6 +23,45 @@ Walker.prototype.walk = function() {
 //display
 
 draw = function() {
+    w.walk();
+    w.display();
+};
+*/
+
+//*****************************
+//*probablity to move in a direction**
+
+// Adapted from Dan Shiffman, natureofcode.com
+/*
+var Walker = function() {
+    this.x = width/2;
+    this.y = height/2;
+};
+// walker display
+Walker.prototype.display = function() {
+    stroke(0, 0, 0);
+    point(this.x, this.y);
+};
+
+// Randomly move up, down, left, right, or stay in one place
+Walker.prototype.walk = function() {
+    var r = random(1);
+
+    // It will be most likely to move to the right
+    if (r < 0.4) {
+      this.x++; //right
+    } else if (r < 0.6) {
+      this.x--;// left
+    } else if (r < 0.8) {
+      this.y++;// up
+    } else {
+      this.y--; // down
+    }
+};
+// create walker
+var w = new Walker();
+
+var draw = function() {
     w.walk();
     w.display();
 };

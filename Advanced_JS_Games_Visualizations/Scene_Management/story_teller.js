f0cb68005e208eb23eb6c23cc92a23cc117d1459
0ly <rolon724@yahoo.com>
2016-07-12 09:07:24 -0400

    Step 1: Make the story respond to mouse clicks

--- Advanced_JS_Games_Visualizations/Scene_Management/story_teller.js
+++ Advanced_JS_Games_Visualizations/Scene_Management/story_teller.js
@@ -2,6 +2,12 @@
 
 We've provided you with, drawScene1, a scene drawing function for the first scene where Leafers is just a seed, and drawScene5, a scene drawing function for the fifth (and last) scene where Leafers is in ultimate form.*/
 
+//Step 1: Make the story respond to mouse clicks
+
+/*Your task for this step is to add logic to the mouseClicked function so that :
+- If the first scene is displayed and you click the mouse, it will display the last scene
+- If the last scene is displayed and you click the mouse, it will display the first scene*/
+
 fill(0, 0, 0);
 textSize(20);
 textAlign(CENTER);
@@ -33,5 +39,10 @@ drawScene1();
 
 mouseClicked=function(){
     
+    if(currentScene === 1 ){
+        drawScene5();
+    }else{
+        drawScene1();
+    } 
 };
 

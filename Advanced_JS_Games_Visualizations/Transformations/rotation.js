72da63c8da8d4fd274ce9caf1c01ee08dbff9c07
0ly <rolon724@yahoo.com>
2016-07-13 12:45:37 -0400

    Step 3: Make the wheel colorful (Complete)

--- Advanced_JS_Games_Visualizations/Transformations/rotation.js
+++ Advanced_JS_Games_Visualizations/Transformations/rotation.js
@@ -10,6 +10,11 @@ Your task for this step is to use a translation to move the upper-left corner of
 //Step 2: Make a wheel
 /*Now, generate a wheel by using 12 rectangles, with each rectangle rotated 30 degrees more than the previous rectangle. To do this, you will use two new components: a for loop and a rotation.*/
 
+//Step 3: Make the wheel colorful
+/*Finally, let's turn the wheel into a color wheel by using a different color for each rectangle.
+
+Make the change in colors, around the color wheel, smooth by incrementing or decrementing each of the r,g, and b values by a constant amount for each rectangle.*/
+
 noStroke();
 background(0, 0, 0);
 var r = 255;
@@ -18,6 +23,10 @@ var b = 0;
 fill(r, g, b);
 
 for(var ang = 0; ang < 360; ang+= 30){
+     fill(r, g, b);
+    r-= 15;
+    g+=5;
+    b+=16;
     pushMatrix();
     translate(200,200);
     rotate(ang);

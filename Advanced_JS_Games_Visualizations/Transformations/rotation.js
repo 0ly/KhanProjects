a8ce8ffffea4b90a67d9cca364170b2efcc50af9
0ly <rolon724@yahoo.com>
2016-07-13 12:36:05 -0400

    Step 2: Make a wheel

--- Advanced_JS_Games_Visualizations/Transformations/rotation.js
+++ Advanced_JS_Games_Visualizations/Transformations/rotation.js
@@ -7,6 +7,9 @@ The program currently draws a rectangle whose upper-left corner is in the upper-
 Your task for this step is to use a translation to move the upper-left corner of the rectangle to the center of the screen.
 */
 
+//Step 2: Make a wheel
+/*Now, generate a wheel by using 12 rectangles, with each rectangle rotated 30 degrees more than the previous rectangle. To do this, you will use two new components: a for loop and a rotation.*/
+
 noStroke();
 background(0, 0, 0);
 var r = 255;
@@ -14,7 +17,10 @@ var g = 90;
 var b = 0;
 fill(r, g, b);
 
-pushMatrix();
-translate(200,200);
-rect(0, 0, 150, 15);
-popMatrix();
+for(var ang = 0; ang < 360; ang+= 30){
+    pushMatrix();
+    translate(200,200);
+    rotate(ang);
+    rect(0, 0, 150, 15);
+    popMatrix();
+}

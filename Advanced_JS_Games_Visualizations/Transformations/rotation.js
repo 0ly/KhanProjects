ea27138c4fc1077a99bde57b66226925b9f6c8ed
0ly <rolon724@yahoo.com>
2016-07-13 12:29:15 -0400

    Rotation (Initial)

---
+++ Advanced_JS_Games_Visualizations/Transformations/rotation.js
@@ -0,0 +1,12 @@
+/*The goal of this challenge is to create a color wheel using many rotated copies of a single rectangle.
+
+The program currently draws a rectangle whose upper-left corner is in the upper-left corner of the screen.*/
+
+noStroke();
+background(0, 0, 0);
+var r = 255;
+var g = 90;
+var b = 0;
+fill(r, g, b);
+
+rect(0, 0, 150, 15);

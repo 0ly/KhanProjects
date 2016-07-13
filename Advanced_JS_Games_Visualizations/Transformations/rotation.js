d71923c6c24711e6823bc30f5a3ee0996ac120bb
0ly <rolon724@yahoo.com>
2016-07-13 12:31:39 -0400

    Step 1: Move the rectangle to the center

--- Advanced_JS_Games_Visualizations/Transformations/rotation.js
+++ Advanced_JS_Games_Visualizations/Transformations/rotation.js
@@ -2,6 +2,11 @@
 
 The program currently draws a rectangle whose upper-left corner is in the upper-left corner of the screen.*/
 
+//Step 1: Move the rectangle to the center
+/*
+Your task for this step is to use a translation to move the upper-left corner of the rectangle to the center of the screen.
+*/
+
 noStroke();
 background(0, 0, 0);
 var r = 255;
@@ -9,4 +14,7 @@ var g = 90;
 var b = 0;
 fill(r, g, b);
 
+pushMatrix();
+translate(200,200);
 rect(0, 0, 150, 15);
+popMatrix();

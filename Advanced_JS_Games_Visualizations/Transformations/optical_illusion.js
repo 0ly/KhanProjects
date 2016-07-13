dde6911891cb75413cfba86347bc3011db38da0b
0ly <rolon724@yahoo.com>
2016-07-13 12:53:40 -0400

    Optical Ilusion (Initial)

---
+++ Advanced_JS_Games_Visualizations/Transformations/optical_illusion.js
@@ -0,0 +1,16 @@
+
+/*In this challenge you will make a cool-looking optical illusion! The illusion will use overlapping black and white circles.*/
+
+var diameter = 600;
+var decreaseAmt = 0.02;
+var scaleF = 1.0;
+
+var drawWhiteCircle = function(diameter) {
+    
+};
+
+var drawBlackCircle = function(diameter) {
+    
+};
+
+background(255, 255, 255);

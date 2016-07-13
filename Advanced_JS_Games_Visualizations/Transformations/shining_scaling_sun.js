e899bfe5fc99c71aeabcff41eb743ebf4d5dc1e9
0ly <rolon724@yahoo.com>
2016-07-13 13:15:33 -0400

    Step 1: Draw sun rays on the sun

--- Advanced_JS_Games_Visualizations/Transformations/shining_scaling_sun.js
+++ Advanced_JS_Games_Visualizations/Transformations/shining_scaling_sun.js
@@ -1,5 +1,10 @@
 /*In this challenge you will draw a sun in the sky with some clouds. The center of the sun is drawn already, but the sun is missing its sun rays!*/
 
+//Step 1: Draw sun rays on the sun
+/*Use a for loop and call the drawSunRay() function to draw the sun rays. You will need to translate and rotate each sun ray, so that they appear in the right spot.
+
+The drawSun() function should draw the entire sun, including both the circle (for the center of the sun) and the sun rays, so make sure that you put your for loop inside the drawSun() function.*/
+
 angleMode = "degrees";
 var backgroundColor = color(135, 206, 250);
 var sunColor = color(255, 255, 0);
@@ -22,6 +27,13 @@ var drawSunRay = function() {
 
 var drawSun = function() {
     //draw sun rays
+     for(var i = 0; i < 360; i+=30){
+        pushMatrix();
+        translate(200,200);
+        rotate(i);
+        drawSunRay();
+        popMatrix();
+    }
     
     
     //draw center of sun

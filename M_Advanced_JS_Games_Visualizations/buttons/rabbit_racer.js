c34f5c96422c8cdf82a66da23dedda947bfb5015
mikeyhavoc <m.jwilliams@outlook.com>
2016-07-18 21:10:13 -0400

    step one description
    step one description to rabbit_racer added.

---
+++ M_Advanced_JS_Games_Visualizations/buttons/rabbit_racer.js
@@ -0,0 +1,10 @@
+//step one
+/*
+You are going to make a small game. In this game, the player races their rabbit (the rightmost rabbit) toward the finish line against 3 other rabbits.
+
+The player's rabbit will be controlled by a button.
+
+Your task, for this step, is to:
+- create a button, btn1, directly beneath the rightmost rabbit
+- make sure the button is drawn on the screen
+*/

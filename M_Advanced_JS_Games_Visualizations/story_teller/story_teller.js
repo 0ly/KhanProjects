210f5996032c0ec76ae7a24cf48d1308007d07c1
mikeyhavoc <m.jwilliams@outlook.com>
2016-07-18 21:03:33 -0400

    step one step two
    step one explanation step two explanation.

---
+++ M_Advanced_JS_Games_Visualizations/story_teller/story_teller.js
@@ -0,0 +1,24 @@
+//step one
+/*
+You're going to tell the story of Leafers, who starts as a seed and grows to Leafer's ultimate form, using scenes.
+
+We've provided you with, drawScene1, a scene drawing function for the first scene where Leafers is just a seed, and drawScene5, a scene drawing function for the fifth (and last) scene where Leafers is in ultimate form.
+
+Your task for this step is to add logic to the mouseClicked function so that :
+- If the first scene is displayed and you click the mouse, it will display the last scene
+- If the last scene is displayed and you click the mouse, it will display the first scene
+*/
+// step two
+/*
+Add the second, third and fourth scenes to fill in the rest of the story.
+
+Each scene should:
+- update the scene index, currentScene, to the scene number
+- draw a unique background
+- display the image of Leafers
+- have text describing the scene
+
+In the second scene Leafers should be a seedling, in the third scene Leafers should be a sapling, and in the fourth scene Leafers should be a tree.
+
+Don't worry about adding logic to mouseClicked to display these scenes in this step. You will do that next step.
+*/
